693ab91177287150d3c1cb27cd08816a54ca2e30
Rafael Nascimento Sampaio <rafaelns@rafaelns.com.br>
2020-09-23 20:44:05 -0300

    Adiciona um segundo ViewController e associa ele ao VC do StoryBoard.
    Adiciona um segundo ViewController ao projeto e associa ele com o ViewController declarado dentro do Storboard.

---
+++ Modulo02/TransicaoTela/TransicaoTela/Views/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  TransicaoTela
+//
+//  Created by Rafael Nascimento Sampaio on 23/09/20.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    
+}
