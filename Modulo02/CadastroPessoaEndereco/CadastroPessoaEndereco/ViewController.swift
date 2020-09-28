3700a5bcf9f0c6d40adb743c237de7006f2420cf
Rafael Nascimento Sampaio <rafaelns@rafaelns.com.br>
2020-09-28 19:58:13 -0300

    Cria novo projeto para cadastro de usuário com endereço
    Cria um novo projeto para prática dos conhecimentos do curso até o momento (Transição de telas, IBOutlets, IBActions e Protocols).

---
+++ Modulo02/CadastroPessoaEndereco/CadastroPessoaEndereco/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CadastroPessoaEndereco
+//
+//  Created by Rafael Nascimento Sampaio on 28/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
